217f0d9f1e4f4071a453ff76d73289ebf7a9b15e
brian <brianarguello358@gmail.com>
2024-08-31 19:58:40 -0400

    base

---
+++ juego.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
+public class juego {
+
+	public static void main(String[] args) {
+
+		Random aleatorio = new Random();
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("***PIEDRA PAPEL O TIJERA***\n");
+
+		System.out.println("[P]iedra, [Pa]pel, [T]ijeras");
+		String[] opciones = {"P", "Pa", "T"};
+		
+		int pcEleccion = aleatorio.nextInt(opciones.length);
+		System.out.println(pcEleccion);
+
+		while (true) {
+
+			System.out.print("elige una de las opciones: ");
+			String eleccion = teclado.nextLine();
+			
+			if (!opciones.contains(eleccion)) {
+
+				System.out.println("\nSolo las opciones mostradas en pantalla\n");
+				continue;	
+
+			}
+			
+			System.out.println("Hola");
+
+		}
+
+		
+
+	}
+
+}
